c719eef103a2215ba1477d0abe41b0643e5c1357
NWSoft <anusit_khuandee@hotmail.com>
2024-05-01 12:12:02 +0700

    fix

--- view/_search.php
+++ view/_search.php
@@ -1,4 +1,6 @@
 <?php
+
+    $allUser = \yii\helpers\ArrayHelper::map(\common\models\AuthUser::findAll(), 'id', 'name');
     $form = \yii\widgets\ActiveForm::begin([
         'action'  => ['index'],
         'method'  => 'get',
@@ -14,7 +16,14 @@
     'data-hide-search' => "true",
     'data-width'       => "150px",
 ]);?>
-    <?=\yii\helpers\Html::activeTextInput($model, 'q[user]', ['class' => 'form-control', 'placeholder' => Yii::t('app', 'User') . '...']);?>
+    <?=\yii\helpers\Html::activeDropDownList($model, 'q[user]', ['' => ''] + $allUser, [
+    'class'            => "form-select form-select-sm rounded-end-0",
+    'data-control'     => "select2",
+    'data-placeholder' => "-" . Yii::t('app', 'Type') . "-",
+    'data-allow-clear' => "true",
+    'data-hide-search' => "true",
+    'data-width'       => "150px",
+]);?>
     <?=\yii\helpers\Html::activeTextInput($model, 'q[search]', ['class' => 'form-control', 'placeholder' => Yii::t('app', 'Search') . '...']);?>
     <button class="btn btn-info" type="submit"><i class="fa fa-search"></i> <?=Yii::t('app', 'Search');?></button>
     <!-- <?=\yii\helpers\Html::button('<i class="fad fa-repeat-1-alt"></i> ' . Yii::t('app', 'Reload'), ['class' => 'btn btn-dark actLoad']);?> -->
